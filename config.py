8cd0985e14698652502b588f6c303f652aafb1ae
Shvab Alexandr <shvab,olexandr@gmail.com>
2026-08-06 09:46:01 +0300

    setup lib

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     jwt_algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
     refresh_token_expire_days: int = 7
+    google_client_id: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
